1eee9fec5d3ef284eb24129ab5aae9e22adb06d6
Priyanshu Rajput <priyanshurajput360@gmail.com>
2026-02-21 10:00:00 +0530

    Fix navbar with proper navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 function Navbar() {
 	return (
-		<nav className="flex flex-row justify-between items-center p-4  bg-gray-900 ">
-			<h1 className="text-3xl font-bold text-gray-100 hover:text-indigo-400">BlockSnipper</h1>
-			<ul className="flex flex-row space-x-4">
-				<li className="text-lg text-gray-200 hover:text-indigo-400 ">
-					<a href="/">Home</a>
+		<nav className="flex flex-row justify-between items-center p-4 bg-gray-900">
+			<h1 className="text-3xl font-bold text-gray-100 hover:text-indigo-400 cursor-pointer">BlockSnipper</h1>
+			<ul className="flex flex-row space-x-6">
+				<li className="text-lg text-gray-200 hover:text-indigo-400 cursor-pointer">
+					Home
 				</li>
-				<li className="text-lg text-gray-200 hover:text-indigo-400">
-					<a href="/">Home</a>
+				<li className="text-lg text-gray-200 hover:text-indigo-400 cursor-pointer">
+					Blocks
 				</li>
-				<li className="text-lg text-gray-200 hover:text-indigo-400">
-					<a href="/">Home</a>
+				<li className="text-lg text-gray-200 hover:text-indigo-400 cursor-pointer">
+					Transactions
 				</li>
 			</ul>
 		</nav>
